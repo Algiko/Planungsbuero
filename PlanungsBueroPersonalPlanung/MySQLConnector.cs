be80697d15882d4f0a124bd5e22fe24d56d056ff
tom.wazlawik <tom.wazlawik@itSchuleStuttgart1.onmicrosoft.com>
2022-03-09 09:00:21 +0100

    test

--- PlanungsBueroPersonalPlanung/MySQLConnector.cs
+++ PlanungsBueroPersonalPlanung/MySQLConnector.cs
@@ -19,7 +19,7 @@ namespace PlanungsBueroPersonalPlanung
         }
 
 
-        private string _DBHost = "localhost";
+        private string _DBHost = "localhost1";
 
         public string DBHost
         {
