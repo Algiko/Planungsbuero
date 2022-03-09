9dc5da0e02bba436a039c5ef2e4b56da4c0e66a1
Tombolii <97587821+Tombolii@users.noreply.github.com>
2022-03-09 08:52:27 +0100

    Add files via upload

---
+++ PlanungsBueroPersonalPlanung/PlanungsBueroPersonalPlanung/DBAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanungsBueroPersonalPlanung
+{
+    class DBAdapter
+
+    {
+        const string dbname = "planungbuero";
+        MySQLConnector connector = new MySQLConnector(dbname);
+
+
+        /////////////Personal///////////////
+        
+
+        public personalHinzufuegen(Personal)
+    }
+}
