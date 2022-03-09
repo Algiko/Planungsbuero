ca078cff29e329bafc3e0699a720d3604ee6cf84
tomwa <tomwa@TOM-LAPTOP>
2022-03-09 08:35:43 +0100

    Projektdateien hinzufügen.

---
+++ PlanungsBueroPersonalPlanung/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+
+namespace PlanungsBueroPersonalPlanung
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MySQLConnector connector = new MySQLConnector("planungsbuero");
+            connector.DBHost = "10.1.13.119";
+            connector.DBUser = "wwi_user";
+            connector
+            
+           
+        }
+
+
+    }
+}
